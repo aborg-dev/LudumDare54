3c02b866718ace4e766726e9d433fd09d5bc9f65
Andrei Kashin <kashin.andrej@gmail.com>
2023-10-02 15:50:49 +0200

    Rename to skyland

--- src/main_menu_screen.rs
+++ src/main_menu_screen.rs
@@ -87,7 +87,7 @@ fn create_main_menu_screen(mut commands: Commands, server: Res<AssetServer>) {
                     // Display the game name
                     parent.spawn(
                         TextBundle::from_section(
-                            "Skyward",
+                            "Skyland",
                             TextStyle {
                                 font: server.load("NotoSerif-SemiBold.ttf"),
                                 font_size: 80.0,
